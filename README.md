90f09893822b9083e72c6d0f7d73e533803016bf
Ankit Rathi <ankit.rathi@inmobi.com>
2021-11-30 11:30:53 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MesonUnityAdapter
+
+A description of this package.
---
+++ Tests/MesonUnityAdapterTests/MesonUnityAdapterTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import MesonUnityAdapter
+
+final class MesonUnityAdapterTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(MesonUnityAdapter().text, "Hello, World!")
+    }
+}
