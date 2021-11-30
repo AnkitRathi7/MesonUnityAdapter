727042210a87aad53d81f266f0f0bea7debd5d29
Ankit Rathi <ankit.rathi@inmobi.com>
2021-11-30 11:46:58 +0530

    dependency

--- Package.swift
+++ Package.swift
@@ -14,6 +14,8 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(name: "MesonSDK", url: "https://github.com/AnkitRathi7/MesonSDK", from: Version(1, 1, 0)),
+        .package(name: "UnityAds", url: "https://github.com/AnkitRathi7/UnityAds", from: Version(1, 0, 0))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
